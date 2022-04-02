862e193aa08616971efe45be466e38171802bbca
Carlos lobo <carlitosami47@gmail.com>
2022-04-01 23:52:22 -0500

    ⚒️FIX: API_URL environment

--- src/app/core/services/films.service.ts
+++ src/app/core/services/films.service.ts
@@ -52,7 +52,7 @@ export class FilmsService {
     if (this.loadingMoreFilms) { return of([]); } // Cargando películas
     this.loadingMoreFilms = true;
     return this.http
-      .get<BillboardResponse>(`${environment.APIURL}/movie/now_playing?`, {
+      .get<BillboardResponse>(`${environment.API_URL}/movie/now_playing?`, {
         params: this.params,
       })
       .pipe(
@@ -68,7 +68,7 @@ export class FilmsService {
     const params = { ...this.params, page: '1', query: text };
 
     return this.http
-      .get<BillboardResponse>(`${environment.APIURL}/search/movie`, {
+      .get<BillboardResponse>(`${environment.API_URL}/search/movie`, {
         params,
       })
       .pipe(map((resp) => resp.results));
@@ -76,7 +76,7 @@ export class FilmsService {
 
   getFilmsDetails = (id: string) => {
     return this.http
-      .get<FilmDetails>(`${environment.APIURL}/movie/${id}`, {
+      .get<FilmDetails>(`${environment.API_URL}/movie/${id}`, {
         params: this.params,
       })
       .pipe(catchError((error) => of(null)));
@@ -84,7 +84,7 @@ export class FilmsService {
 
   getCast = (id: string): Observable<Cast[]> => {
     return this.http
-      .get<CreditsResponse>(`${environment.APIURL}/movie/${id}/credits`, {
+      .get<CreditsResponse>(`${environment.API_URL}/movie/${id}/credits`, {
         params: this.params,
       })
       .pipe(
@@ -98,7 +98,7 @@ export class FilmsService {
     this.loadingMoreFilms = true;
     const params = { ...this.params, page: this.filmByCategory.toString() };
     return this.http
-      .get<BillboardResponse>(`${environment.APIURL}/movie/${category}`, {
+      .get<BillboardResponse>(`${environment.API_URL}/movie/${category}`, {
         params,
       })
       .pipe(
@@ -115,7 +115,7 @@ export class FilmsService {
     const params = { ...this.params, language: 'en-US' };
 
     return this.http
-      .get<TrailerResponse>(`${environment.APIURL}/movie/${id}/videos`, {
+      .get<TrailerResponse>(`${environment.API_URL}/movie/${id}/videos`, {
         params,
       })
       .pipe(
