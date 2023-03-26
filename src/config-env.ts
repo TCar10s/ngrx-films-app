15e9dc3837b6f26507e5027364cbaef41f1de2f1
Carlos Lobo <cslobosilva@gmail.com>
2023-03-25 23:43:02 -0500

    feat:

--- src/config-env.ts
+++ src/config-env.ts
@@ -2,15 +2,21 @@ const fs = require('fs');
 require('dotenv').config();
 const { argv } = require('yargs');
 const environment = argv.environment;
+console.log('Environment: ', environment);
+
 
 let apiURL;
 let targetPath;
 
 if (environment === 'prod') {
   apiURL = process.env.API_KEY;
+  console.log('API_KEY: ', apiURL);
+
   targetPath = `./src/environments/environment.prod.ts`;
 } else {
   apiURL = process.env.API_KEY;
+  console.log('API_KEY Dev: ', apiURL);
+
   targetPath = `./src/environments/environment.ts`;
 }
 
@@ -20,6 +26,7 @@ export const environment = {
     API_KEY: "${apiURL}",
     API_URL: "https://api.themoviedb.org/3"
 }`;
+console.log({envConfigFile});
 
 fs.writeFile(targetPath, envConfigFile, (err: any) => {
   if (err) console.log(err);
